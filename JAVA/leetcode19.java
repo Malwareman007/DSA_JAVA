0a8ac350ac958c4994e69e6e8012c92c62b397fb
Kushal Lalwani <109090706+kushal-lalwani@users.noreply.github.com>
2023-10-19 00:32:21 +0530

    Update leetcode19.java
    Corrected Link

--- JAVA/leetcode19.java
+++ JAVA/leetcode19.java
@@ -1,4 +1,4 @@
-/*LeetCode Problem Link : https://leetcode.com/problems/remove-nth-node-from-end-of-list/solutions/2633379/java-1ms-comments-explained/ */
+/*LeetCode Problem Link : https://leetcode.com/problems/remove-nth-node-from-end-of-list/description/ */
 // Level : Medium
 
 /*Problem : Given the head of a linked list, remove the nth node from the end of the list and return its head.
